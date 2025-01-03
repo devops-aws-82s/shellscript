02b23e141460e1a1097c1b8927a7c83c212b1a7a
prakash <prakashbaireddy@gmail.com>
2025-01-03 15:47:16 +0530

    new changes

---
+++ database.sh
@@ -0,0 +1,53 @@
+#!/bin/bash
+
+USERID=$(id -u)
+
+R="\e[31m"
+G="\e[32m"
+Y="\e[33m"
+N="\e[0m"
+
+mkdir -p /home/ec2-user/expense-logs
+if [ $? -eq 0 ]
+then
+    echo "directory created successfully"
+else
+    echo "directory creation failed"
+fi
+
+LOG_PATH="/home/ec2-user/expense-logs"
+FILE_NAME=$($0 | cut -d "." -f1)
+DATE=$(date +%Y-%m-%d-%H-%M-%S)
+LOG_FILE="$LOG_PATH/$FILE_NAME-$DATE.log"
+
+if [ $USERID -ne 0 ]
+then
+    echo -e " $R You need to root access to run this file $N"
+    exit 1
+fi 
+
+echo "Script started at : $DATE" &>>$LOG_FILE
+
+VALIDATE(){
+if [ $1 -ne 0 ]
+then
+    echo "$2..Failed"
+else
+    echo "$2..Success"
+fi
+}
+
+dnf install mysql-server -y &>>$LOG_FILE
+VALIDATE $? "mysql installing"
+
+systemctl enable mysqld &>>$LOG_FILE
+VALIDATE $? "mysql service enabled"
+
+systemctl start mysqld &>>$LOG_FILE
+VALIDATE $? "mysql service started"
+
+mysql_secure_installation --set-root-pass ExpenseApp@1 &>>$LOG_FILE
+VALIDATE $? "mysql root password creating"
+
+
+
