744e5452b6d32196d3ef2d2fcbdec617a28f660c
prakash <prakashbaireddy@gmail.com>
2025-01-03 20:22:23 +0530

    new changes

--- old-logs.sh
+++ old-logs.sh
@@ -3,11 +3,11 @@
 SOURCE="/home/ec2-user/expense.logs"
 
 FIND=$(find $SOURCE -name "*.log" -mtime +14)
-echo "$FIND"
-rm -rf $FIND
-if [ $? -eq 0 ]
-then 
-    echo "files are deleted"
-else
-    echo "files are not deleted"
-fi
+#echo "$FIND"
+#rm -rf $FIND
+
+while read -r old
+do 
+
+    echo " these files will be deleted: $old"
+done <<<$FIND
