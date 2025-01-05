b8ba762a0485b695e3d771fd05fe13d5711dc587
prakash <prakashbaireddy@gmail.com>
2025-01-05 11:29:27 +0530

    new changes

--- aws-resource.sh
+++ aws-resource.sh
@@ -18,3 +18,9 @@ if [ ! -d ~/.aws ]; then
     echo "AWS CLI is not configured. Please configure the AWS CLI and try again."
     exit 1
 fi
+
+case $aws_service in
+    ec2)
+        echo "Listing EC2 Instances in $aws_region"
+        aws ec2 describe-instances --region $aws_region
+        ;;
