743a2d1d99393258f3c00138b0e72347a00aecb7
prakash <prakashbaireddy@gmail.com>
2025-01-06 19:53:53 +0530

    backup

--- backup.sh
+++ backup.sh
@@ -5,9 +5,20 @@ N="\e[0m"
 
 SOURCE_DIR=$1
 DEST_DIR=$2
+DAYS=${3:-14}
+
 
 if [ $# -lt 2 ]
 then 
     echo " please pass two arguments, USAGE shellscript file name and  <source dir> <dest dir>"
+    exit 1
+fi
+
+LOG_FOLDER="/home/ec2-user/output-logs"
+DATE=$(date +%Y-%m-%d-%H-%M-%S)
+
+if [ ! -d $SOURCE_DIR ]
+then    
+    echo " $SOURCE_DIR not exist"
     exit 1
 fi
