eadc54232692edad800853c7501a10bf0680e0c6
prakash <prakashbaireddy@gmail.com>
2025-01-06 19:44:35 +0530

    backup

--- backup.sh
+++ backup.sh
@@ -8,6 +8,6 @@ DEST_DIR=$2
 
 if [ $# -lt 2 ]
 then 
-    echo "usage of passing parameters <source dir> <dest dir>"
+    echo " please pass two arguments, USAGE shellscript file name and  <source dir> <dest dir>"
     exit 1
 fi
